78a3a4b2554f493628446a3148f97f5d2fdfa937
Zygmut <deltas.game@gmail.com>
2023-02-21 16:58:23 +0100

    Time Complexity & stuff

--- templates/MVC/src/Main.java
+++ templates/MVC/src/Main.java
@@ -1,10 +1,19 @@
+import java.sql.Time;
 
-// import javafx.*;
+import mesurament.Mesurament;
 
 public class Main {
+
+    public void fn() {
+        return;
+    }
+
+    public void master() {
+        // Mesurament.mesura();
+        System.out.println(TimeComplexity.timeIt(this::fn).toNanos());
+    }
+
     public static void main(String[] args) throws Exception {
-        // Mesurament mesura = new Mesurament();
-        // MVC mvc = new MVC();
-        System.out.println("Hello, World!");
+        (new Main()).master();
     }
 }
