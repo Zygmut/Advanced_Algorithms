8fd4210d159108c0d139a4f8ac9e317b5e1e2112
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-03-03 22:12:05 +0100

    Updated main

--- 1_general_concepts/src/Master/Main.java
+++ 1_general_concepts/src/Master/Main.java
@@ -1,9 +1,11 @@
 package Master;
 
+import mesurament.Mesurament;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Mesurament.mesura();
+        Mesurament.mesura();
         new MVC("config.txt").show();
     }
 
