6e9c54cdca652d95d814dfd101a279517ea1fafd
Alejandro Rodríguez <alerodriargui@gmail.com>
2023-03-15 16:18:13 +0100

    Propuesta clase Pieza

--- 2_backtracking/src/Model/Piece.java
+++ 2_backtracking/src/Model/Piece.java
@@ -2,4 +2,49 @@ package Model;
 
 public class Piece {
 
+    private int x;
+    private int y;
+    private int color;
+    private String id;
+
+    public Piece(int x, int y, int color, String id) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.id = id;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getColor() {
+        return this.color;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
 }
