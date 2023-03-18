29458f178276cfcc4b6a46832f175e8791d090a7
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-03-18 10:28:03 +0100

    example debug with mark

--- 2_backtracking/src/Master/Main.java
+++ 2_backtracking/src/Master/Main.java
@@ -5,18 +5,20 @@ import Request.Request;
 import Request.RequestCode;
 import mesurament.Mesurament;
 import java.awt.Point;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // Mesurament.mesura();
         MVC mvc = new MVC("config.txt");
-        ChessBoard board = new ChessBoard(3, 3);
+        ChessBoard board = new ChessBoard(4, 3);
         mvc.getModel().setBoard(board);
 
-        board.addPiece(new Queen(), new Point(0, 0));
-        board.addPiece(new Tower(), new Point(1, 1));
-        System.out.println(board.toString());
+        Point queenpos = new Point(3,2);
+        board.addPiece(new Queen(), queenpos);
+        board.addPiece(new Tower(), new Point(1, 0));
+        Arrays.stream(board.getPieces().getMap().get(queenpos).getMovements(board, queenpos)).forEach(pos -> board.addPiece(new Mark(), pos));
         board.getPieces()
                 .getMap()
                 .entrySet()
@@ -25,7 +27,10 @@ public class Main {
                         piece.getValue().getClass().getSimpleName()
                                 + ": "
                                 + board.getMovementStringAt(piece.getKey())));
-        mvc.notifyRequest(new Request(RequestCode.Start, "main"));
+        System.out.println(board.toString());
+
+
+                                //mvc.notifyRequest(new Request(RequestCode.Start, "main"));
 
     }
 
