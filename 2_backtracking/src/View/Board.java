afc6c8d05ace1407b1281611dd417ac44dead78b
Alejandro Rodríguez <alerodriargui@gmail.com>
2023-03-22 13:01:44 +0100

    update Quitar pieza

--- 2_backtracking/src/View/Board.java
+++ 2_backtracking/src/View/Board.java
@@ -86,6 +86,10 @@ public class Board extends JPanel {
                 public void mouseClicked(MouseEvent evt) {
                     System.out.println("Clicked on " + x + ", " + y);
                     Board.this.setCursor(Cursor.getDefaultCursor());
+                    if (evt.getButton() == MouseEvent.BUTTON3) {
+                        setImagePath("./assets/none.png");
+                        repaint();
+                    }
                 }
             });
         }
