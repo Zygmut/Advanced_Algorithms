fbca5ff91497324492adfdebed29c54f2b17a7ef
Sergi Mayol <sergi2808@gmail.com>
2023-04-09 20:28:26 +0200

    Fix warning body

--- 3_DANDQ/src/Request/Request.java
+++ 3_DANDQ/src/Request/Request.java
@@ -1,17 +1,17 @@
 package Request;
 
-public class Request {
+public class Request<T> {
 	public final RequestCode code;
 	public final String origin;
-	public final Body body;
+	public final Body<T> body;
 
-	public Request(RequestCode code, String origin, Body body) {
+	public Request(RequestCode code, String origin, Body<T> body) {
 		this.code = code;
 		this.origin = origin;
 		this.body = body;
 	}
 
-	public Request(RequestCode code, Object origin, Body body) {
+	public Request(RequestCode code, Object origin, Body<T> body) {
 		this.code = code;
 		this.origin = origin.getClass().getSimpleName();
 		this.body = body;
