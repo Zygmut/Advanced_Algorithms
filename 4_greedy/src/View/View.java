c4c56a138a0e928fade956185e8455778460d2b0
unknown <sergi2808@gmail.com>
2023-04-26 21:29:11 +0200

    Diseño interfaz hecho

--- 4_greedy/src/View/View.java
+++ 4_greedy/src/View/View.java
@@ -1,5 +1,6 @@
 package View;
 
+import java.awt.Color;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -7,8 +8,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
-import Master.MVC;
 import Services.Service;
 import Services.Comunication.Content.Body;
 import Services.Comunication.Request.Request;
@@ -25,12 +30,15 @@ public class View implements Service {
 	 * The window of the view.
 	 */
 	private Window window;
+	/**
+	 * The split pane of the view.
+	 */
+	private JSplitPane splitPane;
 
 	/**
 	 * This constructor creates a view with the MVC hub without any configuration
 	 *
 	 * @param mvc The MVC hub of the view.
-	 * @see MVC
 	 */
 	public View() {
 		this.window = new Window();
@@ -44,7 +52,6 @@ public class View implements Service {
 	 *
 	 * @param mvc        The MVC hub of the view.
 	 * @param configPath The path to its config.
-	 * @see MVC
 	 */
 	public View(String configPath) {
 		this.window = new Window(configPath);
@@ -107,15 +114,99 @@ public class View implements Service {
 	 *
 	 */
 	private void loadContent() {
-		Section demoSection = new Section();
-		JButton demoButton = new JButton("Click me!");
-		demoButton.addActionListener(e -> {
+		this.window.addMenuBar(this.menu());
+		this.initSplitPane();
+		this.splitPane.setRightComponent(this.sideBar());
+		this.window.addSection(this.body(), DirectionAndPosition.POSITION_CENTER, "Body");
+		this.window.addSection(this.footer(), DirectionAndPosition.POSITION_BOTTOM, "Footer");
+	}
+
+	private JPanel sideBar() {
+		JPanel sideBar = new JPanel();
+		sideBar.setBackground(Color.WHITE);
+		JButton button = new JButton("Hello World!");
+		button.addActionListener(e -> {
 			String message = "Hello World!";
 			Request request = new Request(RequestCode.HELLO_WORLD, this, new Body(message));
 			this.sendRequest(request);
 		});
-		demoSection.createButtons(new JButton[] { demoButton }, DirectionAndPosition.DIRECTION_ROW);
-		this.window.addSection(demoSection, DirectionAndPosition.POSITION_TOP, "Demo");
+		sideBar.add(button);
+		return sideBar;
 	}
 
+	private void initSplitPane() {
+		this.splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		this.splitPane.setDividerLocation(0.9);
+		this.splitPane.setResizeWeight(1.0);
+		this.splitPane.setOneTouchExpandable(true);
+		this.splitPane.setBorder(null);
+	}
+
+	private Section body() {
+		JPanel content = new JPanel();
+		content.setBackground(Color.WHITE);
+		splitPane.setLeftComponent(content);
+		Section body = new Section();
+		body.createJSplitPaneSection(splitPane);
+		return body;
+	}
+
+	private Section footer() {
+		Section buttonSection = new Section();
+		JButton[] buttons = new JButton[3];
+		buttons[0] = new JButton("Deshacer");
+		buttons[0].addActionListener(e -> {
+			// TODO
+		});
+		buttons[1] = new JButton("Rehacer");
+		buttons[1].addActionListener(e -> {
+			// TODO
+		});
+		buttons[2] = new JButton("Confirmar");
+		buttons[2].addActionListener(e -> {
+			// TODO
+		});
+		buttonSection.createButtons(buttons, DirectionAndPosition.DIRECTION_ROW);
+		return buttonSection;
+	}
+
+	/**
+	 * This method creates the menu bar of the view.
+	 *
+	 * @return JMenuBar The menu bar of the view.
+	 */
+	private JMenuBar menu() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu options = new JMenu("Opciones");
+		JMenu stats = new JMenu("Estadisticas");
+		JMenuItem alg = new JMenuItem("Algoritmos");
+		alg.addActionListener(e -> {
+			// TODO
+		});
+		JMenuItem jvm = new JMenuItem("JVM");
+		jvm.addActionListener(e -> {
+			WindowJVMStats jvmStats = new WindowJVMStats();
+			jvmStats.show();
+			jvmStats.start();
+		});
+		JMenuItem exit = new JMenuItem("Salir");
+		exit.addActionListener(e -> System.exit(0));
+		stats.add(alg);
+		stats.add(jvm);
+		options.add(stats);
+
+		options.addSeparator();
+		options.add(exit);
+		menuBar.add(options);
+
+		JMenu help = new JMenu("Ayuda");
+		JMenuItem about = new JMenuItem("Manual de Usuario");
+		about.addActionListener(e -> {
+			// TODO
+		});
+		help.add(about);
+		menuBar.add(help);
+
+		return menuBar;
+	}
 }
