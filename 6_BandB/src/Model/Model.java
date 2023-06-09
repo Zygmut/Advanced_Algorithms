c4d199fa279ffefde1f61caaf22223bc6f90a08c
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-06-09 10:31:25 +0200

    Added methods to save and get puzzles and to get stats

--- 6_BandB/src/Model/Model.java
+++ 6_BandB/src/Model/Model.java
@@ -1,8 +1,14 @@
 package Model;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import Services.Service;
 import Services.Comunication.Content.Body;
@@ -17,7 +23,6 @@ public class Model implements Service {
 
 	public Model() {
 		this.dbApi = new DBApi(Config.PATH_TO_DB);
-		// Initialize model things here
 	}
 
 	@Override
@@ -35,13 +40,16 @@ public class Model implements Service {
 	@Override
 	public void notifyRequest(Request request) {
 		switch (request.code) {
-			case GREET -> {
-				Logger.getLogger(this.getClass().getSimpleName())
-						.log(Level.INFO, "Model heard {0} say {1}",
-								new Object[] { request.origin, request.body.content });
-				String message = "Hello " + request.origin + "! I'm the model.";
-				this.saveToDB(message);
-				this.sendResponse(new Response(ResponseCode.GREET_BACK, this, new Body(message)));
+			case SAVE_PUZZLE -> {
+				final Object[] params = (Object[]) request.body.content;
+				final Board board = (Board) params[0];
+				this.savePuzzle(board);
+			}
+			case GET_LAST_PUZZLE -> {
+				this.sendResponse(new Response(ResponseCode.GET_LAST_PUZZLE, this, new Body(getLastPuzzle())));
+			}
+			case FETCH_STATS -> {
+				this.sendResponse(new Response(ResponseCode.FETCH_STATS, this, new Body(getAllSolutions())));
 			}
 			case CREATE_DB -> {
 				Logger.getLogger(this.getClass().getSimpleName())
@@ -55,35 +63,73 @@ public class Model implements Service {
 		}
 	}
 
-	private void saveToDB(String message) {
+	// This can be used for the shuffle and to display the "current" puzzle
+	private Board getLastPuzzle() {
+		Gson gson = new Gson();
 		try {
 			this.dbApi.connect();
-			this.dbApi.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS Greeting(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT);");
-			message = message.replace("'", "''");
-			String query = "INSERT INTO Greeting(message) VALUES('" + message + "');";
-			this.dbApi.executeUpdate(query);
+			ResultSet result = this.dbApi.executeQuery("SELECT * FROM Puzzle ORDER BY id DESC LIMIT 1");
+			if (result.next()) {
+				return gson.fromJson(result.getString("puzzle"), Board.class);
+			}
+
 			this.dbApi.disconnect();
 		} catch (Exception e) {
 			Logger.getLogger(this.getClass().getSimpleName())
 					.log(Level.SEVERE, "Error while saving to DB.", e);
 		}
+
+		return null;
+	}
+
+	// Any time the puzzle is modified (new size, shuffled), the puzzle will need to be solved
+	private void savePuzzle(Board board) {
+		Gson gson = new Gson();
+		try {
+			this.dbApi.connect();
+
+			this.dbApi.executeQuery("INSERT INTO Puzzle (puzzle) VALUES (" + gson.toJson(board) + ")");
+
+			this.dbApi.disconnect();
+		} catch (Exception e) {
+			Logger.getLogger(this.getClass().getSimpleName())
+					.log(Level.SEVERE, "Error while saving to DB.", e);
+		}
+
+	}
+
+	private Solution[] getAllSolutions() {
+		ArrayList<Solution> solutions = new ArrayList<>();
+		final Gson gson = new GsonBuilder()
+				.registerTypeAdapter(Duration.class, new DurationTypeAdapter())
+				.create();
+		try {
+			this.dbApi.connect();
+			ResultSet result = this.dbApi.executeQuery("SELECT * FROM Solution");
+			while (result.next()) {
+				solutions.add(gson.fromJson(result.getString("solution"), Solution.class));
+			}
+
+			this.dbApi.disconnect();
+		} catch (Exception e) {
+			Logger.getLogger(this.getClass().getSimpleName())
+					.log(Level.SEVERE, "Error while saving to DB.", e);
+		}
+
+		return solutions.toArray(Solution[]::new);
 	}
 
 	private void createDB() {
 		try {
 			this.dbApi.connect();
 			this.dbApi.setAutoCommit(false);
+			// This is somewhat redundant, as we will probably save a lot of boards that won't be used.
+			// Anyhow, we can say that this is a feature, as you can generate a lot of boards and use them
+			// in some other program :D
 			this.dbApi.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS Strategy(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT);");
+					"CREATE TABLE IF NOT EXISTS Puzzle(id INTEGER PRIMARY KEY AUTOINCREMENT, puzzle JSON);");
 			this.dbApi.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS RubikCube(id INTEGER PRIMARY KEY AUTOINCREMENT, size INTEGER, configuration TEXT);");
-			this.dbApi.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS RubikCubeStrategy(id INTEGER PRIMARY KEY AUTOINCREMENT, idRubikCube INTEGER, idStrategy INTEGER);");
-			this.dbApi.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS RubikCubeSolution(id INTEGER PRIMARY KEY AUTOINCREMENT, idRubikCube INTEGER, idStrategy INTEGER, solution TEXT, time INTEGER);");
-			this.dbApi.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS RubikCubeSolutionStep(id INTEGER PRIMARY KEY AUTOINCREMENT, idRubikCubeSolution INTEGER, step INTEGER, move TEXT);");
+					"CREATE TABLE IF NOT EXISTS Solution(id INTEGER PRIMARY KEY AUTOINCREMENT, solution JSON);");
 			this.dbApi.commit();
 			this.dbApi.disconnect();
 		} catch (SQLException e) {
