641427c37711764b9448c630ebc4a30531bc773a
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2023-06-09 13:17:28 +0200

    Avoid push database to repo
    Co-authored-by: Sergi Mayol <Sergimayol@users.noreply.github.com>

--- 6_BandB/src/Model/Model.java
+++ 6_BandB/src/Model/Model.java
@@ -1,20 +1,20 @@
 package Model;
 
+import Services.Comunication.Content.Body;
+import Services.Comunication.Request.Request;
+import Services.Comunication.Response.Response;
+import Services.Comunication.Response.ResponseCode;
+import Services.Service;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import Services.Service;
-import Services.Comunication.Content.Body;
-import Services.Comunication.Request.Request;
-import Services.Comunication.Response.Response;
-import Services.Comunication.Response.ResponseCode;
 import utils.Config;
 
 public class Model implements Service {
@@ -27,13 +27,15 @@ public class Model implements Service {
 
 	@Override
 	public void start() {
-		Logger.getLogger(this.getClass().getSimpleName())
+		Logger
+				.getLogger(this.getClass().getSimpleName())
 				.log(Level.INFO, "Model started.");
 	}
 
 	@Override
 	public void stop() {
-		Logger.getLogger(this.getClass().getSimpleName())
+		Logger
+				.getLogger(this.getClass().getSimpleName())
 				.log(Level.INFO, "Model stopped.");
 	}
 
@@ -42,20 +44,27 @@ public class Model implements Service {
 		switch (request.code) {
 			case CALCULATE -> {
 				final Solution sol = (Solution) request.body.content;
-				Logger.getLogger(this.getClass().getSimpleName())
+				Logger
+						.getLogger(this.getClass().getSimpleName())
 						.log(Level.INFO, "Saved Solution to model");
 				this.saveSolution(sol);
 			}
 			case FETCH_STATS -> {
-				this.sendResponse(new Response(ResponseCode.FETCH_STATS, this, new Body(getAllSolutions())));
+				this.sendResponse(
+						new Response(
+								ResponseCode.FETCH_STATS,
+								this,
+								new Body(getAllSolutions())));
 			}
 			case CREATE_DB -> {
 				this.createDB();
-				Logger.getLogger(this.getClass().getSimpleName())
+				Logger
+						.getLogger(this.getClass().getSimpleName())
 						.log(Level.INFO, "Created DB.");
 			}
 			default -> {
-				Logger.getLogger(this.getClass().getSimpleName())
+				Logger
+						.getLogger(this.getClass().getSimpleName())
 						.log(Level.SEVERE, "{0} is not implemented.", request);
 			}
 		}
@@ -67,10 +76,14 @@ public class Model implements Service {
 				.create();
 		try {
 			this.dbApi.connect();
-			this.dbApi.executeUpdate("INSERT INTO Solution (solution) VALUES ('" + gson.toJson(sol) + "')");
+			this.dbApi.executeUpdate(
+					"INSERT INTO Solution (solution) VALUES ('" +
+							gson.toJson(sol) +
+							"')");
 			this.dbApi.disconnect();
 		} catch (Exception e) {
-			Logger.getLogger(this.getClass().getSimpleName())
+			Logger
+					.getLogger(this.getClass().getSimpleName())
 					.log(Level.SEVERE, "Error while saving to DB.", e);
 		}
 	}
@@ -84,12 +97,14 @@ public class Model implements Service {
 			this.dbApi.connect();
 			ResultSet result = this.dbApi.executeQuery("SELECT * FROM Solution");
 			while (result.next()) {
-				solutions.add(gson.fromJson(result.getString("solution"), Solution.class));
+				solutions.add(
+						gson.fromJson(result.getString("solution"), Solution.class));
 			}
 
 			this.dbApi.disconnect();
 		} catch (Exception e) {
-			Logger.getLogger(this.getClass().getSimpleName())
+			Logger
+					.getLogger(this.getClass().getSimpleName())
 					.log(Level.SEVERE, "Error while saving to DB.", e);
 		}
 
@@ -98,6 +113,18 @@ public class Model implements Service {
 
 	private void createDB() {
 		try {
+			File file = new File(Config.PATH_TO_DB);
+			if (!file.exists()) {
+				if (file.createNewFile()) {
+					Logger
+							.getLogger(this.getClass().getSimpleName())
+							.log(Level.INFO, "Created DB file.");
+				} else {
+					Logger
+							.getLogger(this.getClass().getSimpleName())
+							.log(Level.INFO, "DB file already exists.");
+				}
+			}
 			this.dbApi.connect();
 			this.dbApi.setAutoCommit(false);
 			this.dbApi.executeUpdate("DROP TABLE IF EXISTS Solution;");
@@ -106,14 +133,20 @@ public class Model implements Service {
 			this.dbApi.commit();
 			this.dbApi.disconnect();
 		} catch (SQLException e) {
-			Logger.getLogger(this.getClass().getSimpleName())
+			Logger
+					.getLogger(this.getClass().getSimpleName())
 					.log(Level.SEVERE, "Error while creating DB.", e);
 			try {
 				this.dbApi.rollback();
 			} catch (Exception e2) {
-				Logger.getLogger(this.getClass().getSimpleName())
+				Logger
+						.getLogger(this.getClass().getSimpleName())
 						.log(Level.SEVERE, "Error while rolling back DB.", e2);
 			}
+		} catch (IOException e) {
+			Logger
+					.getLogger(this.getClass().getSimpleName())
+					.log(Level.SEVERE, "Error while creating DB file.", e);
 		}
 	}
 }
